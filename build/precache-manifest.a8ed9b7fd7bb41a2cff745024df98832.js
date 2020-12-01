f25824e605868ac1498aad68585c0ab6fb27fde3
Aleksandra Nikiciuk <o.szumarska@gmail.com>
2020-12-01 14:04:36 +0100

    build version added

---
+++ build/precache-manifest.a8ed9b7fd7bb41a2cff745024df98832.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "89ebae791d729802c14d",
+    "url": "/InteractiveMap/static/css/main.26ff5e5b.chunk.css"
+  },
+  {
+    "revision": "89ebae791d729802c14d",
+    "url": "/InteractiveMap/static/js/main.89ebae79.chunk.js"
+  },
+  {
+    "revision": "869caf347dc6764a06d6",
+    "url": "/InteractiveMap/static/js/1.869caf34.chunk.js"
+  },
+  {
+    "revision": "e802153bff115ae5fd93",
+    "url": "/InteractiveMap/static/js/runtime~main.e802153b.js"
+  },
+  {
+    "revision": "adb571162766a085e53a1d15187c00b7",
+    "url": "/InteractiveMap/static/media/d5e9eeb233c29e551f08b79bbf9c6491.adb57116.jpg"
+  },
+  {
+    "revision": "c6782de4e862277b0ac5a877a3cfbbb8",
+    "url": "/InteractiveMap/index.html"
+  }
+];
